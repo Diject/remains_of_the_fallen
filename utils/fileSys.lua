ca33b072cc9e3cf7566086fc1d30a6cbbd254be1
Diject <alexkozlovv@gmail.com>
2024-04-29 20:38:56 +0300

    do not create a directory on each call

--- utils/fileSys.lua
+++ utils/fileSys.lua
@@ -3,6 +3,10 @@ local log = require("diject.remains_of_the_fallen.utils.log")
 ---@class fileSysLib
 local this = {}
 
+local function isdir(path)
+    return lfs.attributes(path, "mode") == "directory"
+end
+
 local function mkdir(path)
     local sep, pStr = package.config:sub(1, 1), ""
     for dir in path:gmatch("[^" .. sep .. "]+") do
@@ -15,10 +19,12 @@ end
 ---@return table<table<integer,string>>
 function this:files(fileExtension)
     local out = {}
-    for fileName in lfs.dir(self.__directory) do
-        local path = self.__directory..fileName
-        if lfs.attributes(path, "mode") == "file" and (not fileExtension or fileName:sub(-fileExtension:len()) == fileExtension) then
-            table.insert(out, {fileName, path})
+    if isdir(self.__directory) then
+        for fileName in lfs.dir(self.__directory) do
+            local path = self.__directory..fileName
+            if lfs.attributes(path, "mode") == "file" and (not fileExtension or fileName:sub(-fileExtension:len()) == fileExtension) then
+                table.insert(out, {fileName, path})
+            end
         end
     end
     return out
@@ -27,10 +33,12 @@ end
 ---@return table<table<integer,string>>
 function this:directories()
     local out = {}
-    for name in lfs.dir(self.__directory) do
-        local path = self.__directory..name
-        if lfs.attributes(path, "mode") == "directory" then
-            table.insert(out, {name, path.."\\"})
+    if isdir(self.__directory) then
+        for name in lfs.dir(self.__directory) do
+            local path = self.__directory..name
+            if lfs.attributes(path, "mode") == "directory" then
+                table.insert(out, {name, path.."\\"})
+            end
         end
     end
     return out
@@ -39,12 +47,13 @@ end
 ---@param fileName string
 ---@param data table
 function this:createTomlFile(fileName, data)
+    mkdir(self.__directory)
     toml.saveFile(self.__directory..fileName, data)
 end
 
 ---@param directoryPath string
 function this:new(directoryPath)
-    mkdir(directoryPath)
+    -- mkdir(directoryPath)
     local out = {}
     setmetatable(out, self)
     self.__index = self
